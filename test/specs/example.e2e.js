a3e118d75097e2bef4b6814f249c05f46ee9eb6f
Iana Pastukhova <monstervan221188@gmail.com>
2021-03-22 14:48:57 -0700

    added profile page and test for auth

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,14 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    it('user logs in with valid data', () => {
         LoginPage.open();
+        LoginPage.setLogin('ianapastukhova22@gmail.com');
+        LoginPage.setPassword('Azazel22#');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
 
-        LoginPage.login('tomsmith', 'SecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
     });
 });
 
